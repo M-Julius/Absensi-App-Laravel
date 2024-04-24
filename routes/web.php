fce4aa1a97049b837ebeef43d935c92f2d022c88
M-Julius <mjulius.dev@gmail.com>
2024-04-24 13:50:04 +0700

    add fortify, login, logout

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.auth-login');
 });
 
 
-Route::get('/dashboard', function () {
-    return view('pages.dashboard', ['type_menu' => 'dashboard']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard', ['type_menu' => 'dashboard']);
+    })->name('home');
+
+    // Route::resource('users', UserController::class);
+    //doctors
+    // Route::resource('doctors', DoctorController::class);
 });
