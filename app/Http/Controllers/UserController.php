08f0346d4a87280fdce4a172ca88e06e7a16230a
M-Julius <mjulius.dev@gmail.com>
2024-04-25 08:30:56 +0700

    feat: add user management

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    //
+    public function index()
+    {
+        // search by name, pagination 10
+        $users = User::where('name', 'like', '%'.request('name').'%')
+        ->orderBy('id', 'desc')
+        ->paginate(10);
+        return view('pages.users.index', compact('users'));
+    }
+
+    // create
+    public function create()
+    {
+        return view('pages.users.create');
+    }
+
+    // store
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required',
+            'role' => 'required',
+            'password' => 'required|min:8',
+        ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->phone = $request->phone;
+        $user->role = $request->role;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return redirect()->route('users.index')->with('success', 'User created successfully.');
+
+    }
+
+    // edit
+    public function edit(User $user)
+    {
+        return view('pages.users.edit', compact('user'));
+    }
+
+    // update
+    public function update(Request $request, User $user)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required',
+            'role' => 'required',
+        ]);
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->phone = $request->phone;
+        $user->role = $request->role;
+
+        if($request->password)
+        {
+            $user->password = Hash::make($request->password);
+        }
+
+        $user->save();
+
+        return redirect()->route('users.index')->with('success', 'User updated successfully.');
+    }
+
+    // destroy
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return redirect()->route('users.index')->with('success', 'User deleted successfully.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,6 +13,7 @@ Route::middleware(['auth'])->group(function () {
         return view('pages.dashboard', ['type_menu' => 'dashboard']);
     })->name('home');
 
+    Route::resource('users', UserController::class);
     // Route::resource('users', UserController::class);
     //doctors
     // Route::resource('doctors', DoctorController::class);
